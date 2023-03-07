4ce495e76c04a66cdae76e8155f51eb2aa868cf9
INODE64 <ffelix@inode64.com>
2023-03-07 12:51:20 +0100

    Add opt_parse

--- lib/smms/applications.bash
+++ lib/smms/applications.bash
@@ -1,1 +1,23 @@
 #!/usr/bin/env bash
+
+applications_version() {
+	$(WHICH "$1") --version
+}
+
+applications_v() {
+	$(WHICH "$1") -v
+}
+
+applications_vv() {
+	$(WHICH "$1") -V
+}
+
+applications_status() {
+	applications_version "$1" &>/dev/null
+}
+
+SMMS_APLICATIONS=()
+for app in ${SMMS_ROOT}/libexec/smms/applications/*; do
+	SMMS_APLICATIONS+=( "$(basename "${app}")" )
+	source "${app}"
+done
